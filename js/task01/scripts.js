1be1f431e92540370f992d08bca8d028133b1d5a
Ta Quoc Anh <anh.taquoc@cisbox.com>
2024-10-27 22:42:39 +0700

    TASK 1 SUBMIT

---
+++ js/task01/scripts.js
@@ -0,0 +1,24 @@
+import createCard from "./card_generator.js"
+
+const CONUTRY_CODE = ["DE","VN"]
+const cardContainer = document.getElementById(`card-container`);
+const processData = async (code) => {
+  const url = `https://restcountries.com/v3.1/alpha/${code}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    cardContainer.innerHTML += createCard(json[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+$(document).ready(() => {
+  CONUTRY_CODE.forEach((code) => {
+    processData(code)
+  });
+})
